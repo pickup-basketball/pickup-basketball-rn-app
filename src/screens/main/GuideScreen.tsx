216ea0e9d6794424a9d1e96a882ea3c665b2fd7e
cyweyo <cyw7715@gmail.com>
2025-01-24 13:45:59 +0900

    가이드 스크린 추가

--- src/screens/main/GuideScreen.tsx
+++ src/screens/main/GuideScreen.tsx
@@ -1,1 +1,41 @@
-// 가이드 화면
+import LoggedInHeader from "../../components/common/LoggedInHeader";
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import {
+  GuideHeader,
+  QuickGuide,
+  DetailGuide,
+  FaqSection,
+  ContactSection,
+} from "../../components/main/GuidItem";
+
+const GuideScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <LoggedInHeader />
+      <ScrollView>
+        <GuideHeader />
+        <QuickGuide />
+        <DetailGuide />
+        <FaqSection />
+        <ContactSection />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+});
+
+export default GuideScreen;
