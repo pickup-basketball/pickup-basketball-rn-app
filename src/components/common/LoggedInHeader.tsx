39d002b994ae7b38c0ee73914ec842673c59fecc
cyweyo <cyw7715@gmail.com>
2025-01-25 18:18:02 +0900

    가이드 버튼 삭제

--- src/components/common/LoggedInHeader.tsx
+++ src/components/common/LoggedInHeader.tsx
@@ -1,27 +1,36 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../screens/auth/SignupScreen";
+import { User } from "lucide-react-native";
+import { colors } from "../../styles/colors";
 
 type TNavigationProp = StackNavigationProp<RootStackParamList>;
 
 const LoggedInHeader = () => {
   const navigation = useNavigation<TNavigationProp>();
   return (
-    <View style={styles.header}>
-      <Text style={styles.logo}>PICKUP</Text>
-
-      <TouchableOpacity onPress={() => navigation.navigate("Guide")}>
-        <Text style={styles.menuLinkText}>가이드</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Matching")}>
-        <Text style={styles.menuLinkText}>매칭</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Courts")}>
-        <Text style={styles.menuLinkText}>코트</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.navigate("Guide")}>
+          <Text style={styles.logo}>PICKUP</Text>
+        </TouchableOpacity>
+        <View style={styles.menuButton}>
+          <TouchableOpacity onPress={() => navigation.navigate("Matching")}>
+            <Text style={styles.menuLinkText}>매칭</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate("Courts")}>
+            <Text style={styles.menuLinkText}>코트</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.userButton}>
+          <TouchableOpacity>
+            <User color="white" size={22} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -30,27 +39,27 @@ export default LoggedInHeader;
 const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
     padding: 20,
-    paddingBottom: 0,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "black",
+    justifyContent: "space-between",
   },
   logo: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#fff",
+    fontSize: 30,
+    fontWeight: "900",
+    color: "white",
   },
-  buttonContent: {
+  menuButton: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
-  },
-  mainButtonText: {
-    color: "#fff",
-    fontSize: 16,
+    gap: 40,
   },
   menuLinkText: {
     color: "white",
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: "bold",
   },
+  userButton: {},
 });
