d24d2bb08f7297610630d5359fc9887001b732f1
cyweyo <cyw7715@gmail.com>
2025-02-05 09:33:01 +0900

    메인 탭 기능 추가

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -1,13 +1,102 @@
-// 메인 탭 네이게이션
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import CourtsScreen from "../screens/main/CourtsScreen";
+import GuideScreen from "../screens/main/GuideScreen";
+import { MatchingScreen } from "../screens/main/MatchingScreen";
+import { MyPageScreen } from "../screens/main/MyPageScreen";
+import WriteMatchForm from "../screens/main/WriteMatchFormScreen";
+
+// 스택 네비게이터 생성
+const GuideStack = createStackNavigator();
+const MatchingStack = createStackNavigator();
+const CourtsStack = createStackNavigator();
+const MyPageStack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const MainTabNavigator = () => {
+// Guide 스택
+const GuideStackNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Guide" component={GuideScreen} />
-      <Tab.Screen name="Matching" component={MatchingScreen} />
-      <Tab.Screen name="Court" component={CourtScreen} />
+    <GuideStack.Navigator>
+      <GuideStack.Screen
+        name="GuideMain"
+        component={GuideScreen}
+        options={{ headerShown: false }}
+      />
+    </GuideStack.Navigator>
+  );
+};
+
+// Matching 스택
+const MatchingStackNavigator = () => {
+  return (
+    <MatchingStack.Navigator>
+      <MatchingStack.Screen
+        name="MatchingMain"
+        component={MatchingScreen}
+        options={{ headerShown: false }}
+      />
+      <MatchingStack.Screen
+        name="WriteMatch" // 매치 작성 화면 추가
+        component={WriteMatchForm}
+        options={{ headerShown: false }}
+      />
+    </MatchingStack.Navigator>
+  );
+};
+
+// Court 스택
+const CourtsStackNavigator = () => {
+  return (
+    <CourtsStack.Navigator>
+      <CourtsStack.Screen
+        name="CourtMain"
+        component={CourtsScreen}
+        options={{ headerShown: false }}
+      />
+    </CourtsStack.Navigator>
+  );
+};
+
+// MyPage 스택
+const MypageStackNavigator = () => {
+  return (
+    <MyPageStack.Navigator>
+      <MyPageStack.Screen
+        name="MyPageMain"
+        component={MyPageScreen}
+        options={{ headerShown: false }}
+      />
+    </MyPageStack.Navigator>
+  );
+};
+
+export const MainTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { display: "none" },
+      }}
+    >
+      <Tab.Screen
+        name="Guide"
+        component={GuideStackNavigator}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Matching"
+        component={MatchingStackNavigator}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Courts"
+        component={CourtsStackNavigator}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="MyPage"
+        component={MypageStackNavigator}
+        options={{ headerShown: false }}
+      />
     </Tab.Navigator>
   );
 };
